405ac2c6c445720be8f533b071b7328eb92a9aad
lurq <18811753620@163.com>
2019-12-01 23:23:53 +0800

    add init function

--- model.py
+++ model.py
@@ -2,6 +2,14 @@ import torch
 from torch import nn
 
 
+def weights_init_linear(m):
+    classname = m.__class__.__name__
+    if classname.find('Linear') != -1:
+        nn.init.normal_(m.weight, std=0.001)
+        if m.bias is not None:
+            nn.init.constant_(m.bias, 0.0)
+
+
 class BPnet(nn.Module):
     def __init__(self, num_layers, in_planes, mid_planes):
         super(BPnet, self).__init__()
@@ -19,6 +27,7 @@ class BPnet(nn.Module):
         self.mid_layer = nn.Sequential(*layers)
         self.out_layer = nn.Linear(self.mid_planes, 1, bias=True)
      
+        self.apply(weights_init_linear)
       
     def forward(self, x):
         out = self.in_layer(x)
@@ -26,5 +35,4 @@ class BPnet(nn.Module):
         out = self.mid_layer(out)
         out = self.out_layer(out)
         return out
-        
-    
+            
