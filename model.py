c24af0f7e1597c3db7d9879babdec57e3eeb25f4
lurq <18811753620@163.com>
2019-12-22 21:06:13 +0800

    flexible activation function

--- model.py
+++ model.py
@@ -11,19 +11,21 @@ def weights_init_linear(m):
 
 
 class BPnet(nn.Module):
-    def __init__(self, num_layers, in_planes, mid_planes):
+    def __init__(self, num_layers, in_planes, 
+                 mid_planes, activation_type='relu'):
         super(BPnet, self).__init__()
        
         self.num_layers = num_layers
         self.in_planes = in_planes
         self.mid_planes = mid_planes
+        self.activation = nn.ReLU if activation_type is 'relu' else nn.Sigmoid
          
         self.in_layer = nn.Linear(self.in_planes, self.mid_planes, bias=True)
-        self.relu_in = nn.ReLU(inplace=True)
+        self.in_activation = self.activation()
         layers = []
         for i in range(num_layers-1):
             layers.append(nn.Linear(self.mid_planes, self.mid_planes, bias=True))
-            layers.append(nn.ReLU(inplace=True))
+            layers.append(self.activation())
         self.mid_layer = nn.Sequential(*layers)
         self.out_layer = nn.Linear(self.mid_planes, 1, bias=True)
      
@@ -31,7 +33,7 @@ class BPnet(nn.Module):
       
     def forward(self, x):
         out = self.in_layer(x)
-        out = self.relu_in(out)
+        out = self.in_activation(out)
         out = self.mid_layer(out)
         out = self.out_layer(out)
         return out
