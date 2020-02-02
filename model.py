eb02ce6f8031b89783588232697648a799ca03b6
lurq <18811753620@163.com>
2020-02-02 18:07:34 +0800

    add dropout and tanh activation, and use xavier init method

--- model.py
+++ model.py
@@ -15,6 +15,17 @@ def weights_init_kaiming(m):
         nn.init.kaiming_normal_(m.weight, a=0, mode='fan_out')
         nn.init.constant_(m.bias, 0.0)
 
+def weights_init_xavier(m):
+    classname = m.__class__.__name__
+    if classname.find('Linear') != -1:
+        nn.init.xavier_normal_(m.weight, gain=1)
+        nn.init.constant_(m.bias, 0.0)
+
+
+activation_dict = {'relu': nn.ReLU, 
+                   'sigmoid': nn.Sigmoid,
+                   'tanh': nn.Tanh}
+
 
 class BPnet(nn.Module):
     def __init__(self, num_layers, in_planes, 
@@ -24,22 +35,24 @@ class BPnet(nn.Module):
         self.num_layers = num_layers
         self.in_planes = in_planes
         self.mid_planes = mid_planes
-        self.activation = nn.ReLU() if activation_type is 'relu' else nn.Sigmoid()
+        self.activation = activation_dict[activation_type]()
+        self.dropout = nn.Dropout(p=0.6)
          
         self.in_layer = nn.Linear(self.in_planes, self.mid_planes, bias=True)
         layers = []
         for i in range(num_layers-1):
             layers.append(nn.Linear(self.mid_planes, self.mid_planes, bias=True))
             layers.append(self.activation)
+            layers.append(self.dropout)
         self.mid_layer = nn.Sequential(*layers)
         self.out_layer = nn.Linear(self.mid_planes, 1, bias=True)
      
-        self.apply(weights_init_kaiming)
-        self.out_layer.apply(weights_init_classifier)
+        self.apply(weights_init_xavier)
       
     def forward(self, x):
         out = self.in_layer(x)
         out = self.activation(out)
+        out = self.dropout(out)
         out = self.mid_layer(out)
         out = self.out_layer(out)
         return out
