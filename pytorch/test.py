c0ac7c30e078188ccad4b14b94eb593cf6a57bbd
lurq <18811753620@163.com>
2020-03-09 13:47:36 +0800

    mv 95CI and roc scripts to tools folder

--- pytorch/test.py
+++ pytorch/test.py
@@ -59,23 +59,6 @@ def test(config, output_dir, test_weight, plot_roc=False):
     return auc, optimum, all_acc
 
 
-def calculate_95CI(data, return_idx=False):
-    if len(data.shape) == 1:
-        data = data[np.newaxis, :]
-    trial_num = data.shape[1]
-    interval = int(np.ceil(trial_num * 0.25))
-   
-    idx = np.argsort(data, axis=1)
-    med = data[np.arange(data.shape[0]), \
-               idx[:, int(np.floor(trial_num / 2.0))]]
-    sorted_data = np.sort(data, axis=1)
-    CI_95 = sorted_data[:, (interval, trial_num-interval-1)]
-    
-    if return_idx:
-        return med, CI_95, idx[:, int(np.floor(trial_num/2.0))]
-    return med, CI_95
-    
-    
 def multi_test(cfg, output_dir):
     logger = logging.getLogger('clinic')
     logger.setLevel(logging.DEBUG)
